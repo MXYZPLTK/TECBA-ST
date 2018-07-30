13211ebc0c47f2a3b058573c156299b2ab566c5e
MXYZPLTK <mxyzpltk2048@gmail.com>
2018-07-30 10:46:59 -0400

    V 1.2

--- src/main/java/com/TECBA/puntosdeinteres/demo/Admin.java
+++ src/main/java/com/TECBA/puntosdeinteres/demo/Admin.java
@@ -4,4 +4,36 @@ public class Admin {
     private String name;
     private String id;
     private String rol;
+
+    public Admin(String name, String id, String rol) {
+        this.name = name;
+        this.id = id;
+        this.rol = rol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+
 }
