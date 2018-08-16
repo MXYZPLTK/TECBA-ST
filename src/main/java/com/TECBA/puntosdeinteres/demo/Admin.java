bab728f85adfc48d8f0f7310581fb2dcaaf6ebd3
MXYZPLTK <rebornnxd@gmail.com>
2018-08-16 15:27:11 -0400

    3.2

--- src/main/java/com/TECBA/puntosdeinteres/demo/Admin.java
+++ src/main/java/com/TECBA/puntosdeinteres/demo/Admin.java
@@ -31,42 +31,30 @@ public class Admin extends Cuenta {
         return usuarioEncontrado.get(0);
     }
 
+    public String updateUser(String userToUpdate, String name, String address) {
+        String newName = "";
+        for (int i = 0; i < listaUsuarios.size(); i++) {
+            if (listaUsuarios.get(i).getName().equals(userToUpdate)) {
+                listaUsuarios.get(i).setName(name);
+                listaUsuarios.get(i).setAdress(address);
+                newName = listaUsuarios.get(i).getName();
+            } else {
+                System.out.print("No existe el usuario");
+            }
+        }
+        return newName;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public String deleteUser(String user){
+        String mensaje="";
+        for(int i=0; i<listaUsuarios.size();i++){
+            if(listaUsuarios.get(i).getName().equals(user)){
+                listaUsuarios.remove(i);
+                mensaje="Usuario borrado";
+            }
+            else
+                mensaje="Usuario no encontrado";
+        }
+        return mensaje;
+    }
 }
--- src/test/java/com/TECBA/puntosdeinteres/demo/AdminTest.java
+++ src/test/java/com/TECBA/puntosdeinteres/demo/AdminTest.java
@@ -22,6 +22,23 @@ public class  AdminTest{
         assertEquals( user1, admin1.readUser("Jorge"));
 
     }
+    @Test
+    public void updateUser(){
+        Admin admin1 = new Admin("Raul", 15,"coso@gmail.com","asdsads av.", 44554, "raulin", "12345");
+        Usuario user1 = new Usuario("Jorge", 18,"Coso@gmail.com", "15 av", 4453);
+        admin1.createUser(user1);
+        assertEquals("Mario", admin1.updateUser(user1.getName(), "Mario", "17 av"));
+    }
+
+    @Test
+    public void deleteUser(){
+        Admin admin1 = new Admin("Raul", 15,"coso@gmail.com","asdsads av.", 44554, "raulin", "12345");
+        Usuario user1 = new Usuario("Jorge", 18,"Coso@gmail.com", "15 av", 4453);
+        admin1.createUser(user1);
+        assertEquals("Usuario borrado", admin1.deleteUser("Jorge"));
+
+
+    }
 
     }
 
